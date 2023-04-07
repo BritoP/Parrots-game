480bac1f20e979897724a13e1241bb563689fc1f
BritoP <britop@alunos.utfpr.edu.br>
2023-04-07 02:56:16 -0300

    Prompt inicial feito

---
+++ script.js
@@ -0,0 +1,7 @@
+let numeroCartasPrompt = 0;
+
+numeroCartasPrompt = parseInt(window.prompt("Com quantas cartas você vai jogar? (Min = 4 Max = 14)"));
+console.log(numeroCartasPrompt);
+
+const arrayCartas = new Array(numeroCartasPrompt);
+
